78e0f206870b016d6815937db1505e9eb923ed2c
MangledMan <Themangle906@gmail.com>
2022-09-01 09:48:34 -0500

    Yes

---
+++ src/AsciiArt.java
@@ -0,0 +1,14 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("  _____");
+        System.out.println("  |   |");
+        System.out.println("  |   |");
+        System.out.println("  |   |");
+        System.out.println("----------");
+        System.out.println(" _______");
+        System.out.println(" |     |");
+        System.out.println(" | 0 0 |");
+        System.out.println(" | ___ |");
+        System.out.println("  -----");
+    }
+}
